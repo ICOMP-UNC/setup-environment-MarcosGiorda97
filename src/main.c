50d955428c93d13da85ad78bafddfcea29e266e1
MarcosGiorda97 <marcos.giorda@unc.edu.ar>
2024-03-21 22:07:16 -0300

    Add  and link lib

--- src/main.c
+++ src/main.c
@@ -6,5 +6,9 @@ int main(int argc, char* argv[])
     printf("Hello OS II\n");
     greetings();
 
+    printf("....\n");
+    printf("Calling shared lib from main program. I get '%d'\n",shared_lib_function("Hello, OS II.."));
+    printf("Calling static lib from main program. I get '%d'\n",static_lib_function("Bye bye, OS II.."));
+
     return 0;
 }
